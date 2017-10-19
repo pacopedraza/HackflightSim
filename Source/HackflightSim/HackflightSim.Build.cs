952e81c6c795442c644e233c601bb64648658498
simondlevy <simon.d.levy@gmail.com>
2017-10-19 16:59:48 -0400

    Fixed typo in header

--- Source/HackflightSim/HackflightSim.Build.cs
+++ Source/HackflightSim/HackflightSim.Build.cs
@@ -15,7 +15,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
    You should have received a copy of the GNU General Public License
-   along with EM7180.  If not, see <http://www.gnu.org/licenses/>.
+   along with HackflightSim.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using UnrealBuildTool;
