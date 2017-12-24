642d39e3eb1e98fc4fea5e8f51103acf144dca7e
Simon D. Levy <simon.d.levy@gmail.com>
2017-12-24 16:51:23 -0500

    Added header comments

--- Source/HackflightSim/HackflightSimCameraHUD.h
+++ Source/HackflightSim/HackflightSimCameraHUD.h
@@ -1,4 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+   HackflihtSimCameraHUD.hpp: heads-up display class definition
+
+   Copyright (C) Simon D. Levy 2017
+
+   This file is part of HackflightSim.
+
+   HackflightSim is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   HackflightSim is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
